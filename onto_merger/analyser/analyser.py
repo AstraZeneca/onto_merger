7c72bf3aa59652e8dcfae2ae3dd4ca8b85091edc
dgeleta <87314126+dgeleta@users.noreply.github.com>
2022-05-17 13:58:59 +0100

    output data analysis

--- onto_merger/analyser/analyser.py
+++ onto_merger/analyser/analyser.py
@@ -1,14 +1,22 @@
+from typing import Union
+
 import pandas as pd
 from pandas import DataFrame
 
 from onto_merger.analyser.analysis_util import produce_table_with_namespace_column_for_node_ids, \
-    produce_table_node_namespace_distribution
-from onto_merger.alignment.hierarchy_utils import produce_node_id_table_from_edge_table
+    produce_table_node_namespace_distribution, produce_table_with_namespace_column_pair
+from onto_merger.alignment.hierarchy_utils import produce_node_id_table_from_edge_table, \
+    get_namespace_column_name_for_column
 from onto_merger.data.constants import SCHEMA_NODE_ID_LIST_TABLE, COLUMN_DEFAULT_ID, COLUMN_COUNT, \
-    SCHEMA_HIERARCHY_EDGE_TABLE, COLUMN_PROVENANCE, COLUMN_RELATION, COLUMN_SOURCE_ID
+    SCHEMA_HIERARCHY_EDGE_TABLE, COLUMN_PROVENANCE, COLUMN_RELATION, COLUMN_SOURCE_ID, COLUMN_TARGET_ID, \
+    DIRECTORY_INPUT, COLUMN_SOURCE_TO_TARGET, DIRECTORY_OUTPUT
 
 TEST_PATH = "/Users/kmnb265/Desktop/test_data"
+TEST_PATH_DOM = "/Users/kmnb265/Desktop/test_data/output/domain_ontology"
+OUTPUT_PATH = f"{TEST_PATH}/output/intermediate/analysis/"
 COVERED = "covered"
+
+from onto_merger.report import plot_generator
 
 
 def load_csv(table_path: str) -> DataFrame:
@@ -105,80 +113,204 @@ def produce_node_analysis(nodes: DataFrame, mappings: DataFrame, edges_hierarchy
 
 
 def produce_mapping_analysis_for_type(mappings: DataFrame) -> DataFrame:
-    df = mappings[[COLUMN_RELATION, COLUMN_SOURCE_ID]].groupby([COLUMN_RELATION])\
-        .count()\
-        .reset_index()\
-        .rename(columns={COLUMN_SOURCE_ID: COLUMN_COUNT})\
+    df = mappings[[COLUMN_RELATION, COLUMN_PROVENANCE, COLUMN_SOURCE_ID]].groupby([COLUMN_RELATION]) \
+        .agg(count=(COLUMN_SOURCE_ID, 'count'),
+             provs=(COLUMN_PROVENANCE, lambda x: set(x))) \
+        .reset_index() \
         .sort_values(COLUMN_COUNT, ascending=False)
     print_df_stats(df, "type")
     return df
 
-def produce_mapping_analysis_for_type_and_prov(mappings: DataFrame) -> DataFrame:
-    df = mappings[[COLUMN_RELATION, COLUMN_PROVENANCE, COLUMN_SOURCE_ID]].groupby([COLUMN_RELATION, COLUMN_PROVENANCE])\
-        .count()\
-        .reset_index()\
-        .rename(columns={COLUMN_SOURCE_ID: COLUMN_COUNT})\
-        .sort_values(COLUMN_COUNT, ascending=False)
-    print_df_stats(df, "type and prov")
-    return df
-
-def produce_mapping_analysis_for_prov_and_type(mappings: DataFrame) -> DataFrame:
-    df = mappings[[COLUMN_RELATION, COLUMN_PROVENANCE, COLUMN_SOURCE_ID]].groupby([COLUMN_PROVENANCE, COLUMN_RELATION])\
-        .count()\
-        .reset_index()\
-        .rename(columns={COLUMN_SOURCE_ID: COLUMN_COUNT})\
-        .sort_values(COLUMN_COUNT, ascending=False)
-    print_df_stats(df, "prob and type")
-    return df
-
 
 def produce_mapping_analysis_for_prov(mappings: DataFrame) -> DataFrame:
-    df = mappings[[COLUMN_PROVENANCE, COLUMN_SOURCE_ID]].groupby([COLUMN_PROVENANCE])\
-        .count()\
-        .reset_index()\
-        .rename(columns={COLUMN_SOURCE_ID: COLUMN_COUNT})\
+    df = mappings[[COLUMN_RELATION, COLUMN_PROVENANCE, COLUMN_SOURCE_ID]].groupby([COLUMN_PROVENANCE]) \
+        .agg(count=(COLUMN_SOURCE_ID, 'count'),
+             relations=(COLUMN_RELATION, lambda x: set(x))) \
+        .reset_index() \
         .sort_values(COLUMN_COUNT, ascending=False)
-    print_df_stats(df, "prov")
+    print_df_stats(df, "type")
     return df
 
 
 def produce_mapping_analysis_for_mapped_nss(mappings: DataFrame) -> DataFrame:
-    pass
+    col_nss_set = 'nss_set'
+    df = produce_table_with_namespace_column_for_node_ids(table=mappings)
+    df[col_nss_set] = df.apply(
+        lambda x: str({x[get_namespace_column_name_for_column(COLUMN_SOURCE_ID)],
+                       x[get_namespace_column_name_for_column(COLUMN_TARGET_ID)]}),
+        axis=1
+    )
+    df = df[[col_nss_set, COLUMN_RELATION, COLUMN_PROVENANCE, COLUMN_SOURCE_ID]] \
+        .groupby([col_nss_set]) \
+        .agg(count=(COLUMN_SOURCE_ID, 'count'),
+             types=(COLUMN_RELATION, lambda x: set(x)),
+             provs=(COLUMN_PROVENANCE, lambda x: set(x))) \
+        .reset_index() \
+        .sort_values(COLUMN_COUNT, ascending=False)
+    print_df_stats(df, "nss")
+    return df
 
 
-def produce_mapping_analysis(mappings: DataFrame) -> DataFrame:
+def produce_edges_analysis_for_mapped_or_connected_nss_heatmap(edges: DataFrame,
+                                                               prune: bool = False,
+                                                               directed_edge: bool = False):
+    cols = [get_namespace_column_name_for_column(COLUMN_SOURCE_ID),
+            get_namespace_column_name_for_column(COLUMN_TARGET_ID)]
+    df = produce_table_with_namespace_column_for_node_ids(table=edges)
+    df: DataFrame = df.groupby(cols).agg(count=(COLUMN_SOURCE_ID, 'count')) \
+        .reset_index().sort_values(COLUMN_COUNT, ascending=False)
 
-    # mappings_with_ns = produce_table_with_namespace_column_for_node_ids(mappings)
+    # matrix
+    namespaces = sorted(list(set((df[cols[0]].tolist() + df[cols[1]].tolist()))))
+    matrix = [[0 for ns in namespaces] for ns in namespaces]
+    for _, row in df.iterrows():
+        src_ns = row[cols[0]]
+        trg_ns = row[cols[1]]
+        count = row[COLUMN_COUNT]
+        current_value1 = matrix[namespaces.index(src_ns)][namespaces.index(trg_ns)]
+        matrix[namespaces.index(src_ns)][namespaces.index(trg_ns)] += current_value1 + count
+        if directed_edge is False:
+            current_value2 = matrix[namespaces.index(trg_ns)][namespaces.index(src_ns)]
+            matrix[namespaces.index(trg_ns)][namespaces.index(src_ns)] += current_value2 + count
+    matrix_df = pd.DataFrame(matrix, columns=namespaces, index=namespaces)
 
-    analysis_for_prov = produce_mapping_analysis_for_prov(mappings=mappings)
-    analysis_for_type = produce_mapping_analysis_for_type(mappings=mappings)
-    # analysis_for_type_prov = produce_mapping_analysis_for_type_and_prov(mappings=mappings)
-    # analysis_for_prov_type = produce_mapping_analysis_for_prov_and_type(mappings=mappings)
+    # prune 0s
+    if prune is True:
+        matrix_df = matrix_df.loc[~(matrix_df == 0).all(axis=1)]
+        matrix_df = matrix_df.loc[:, (matrix_df != 0).any(axis=0)]
+
+    return matrix_df
 
 
+def produce_hierarchy_edge_analysis_for_mapped_nss(edges: DataFrame) -> DataFrame:
+    df = produce_table_with_namespace_column_pair(
+        table=produce_table_with_namespace_column_for_node_ids(table=edges)) \
+        .groupby([COLUMN_SOURCE_TO_TARGET]) \
+        .agg(count=(COLUMN_SOURCE_ID, 'count'),
+             provs=(COLUMN_PROVENANCE, lambda x: set(x))) \
+        .reset_index() \
+        .sort_values(COLUMN_COUNT, ascending=False)
+    df["freq"] = df.apply(
+        lambda x: ((x[COLUMN_COUNT] / len(edges)) * 100), axis=1
+    )
+    print_df_stats(df, "hierarchy_edge")
+    return df
+
+
+def produce_merge_analysis_for_merged_nss(merges: DataFrame) -> DataFrame:
+    cols = [get_namespace_column_name_for_column(COLUMN_SOURCE_ID),
+            get_namespace_column_name_for_column(COLUMN_TARGET_ID)]
+    df = produce_table_with_namespace_column_pair(
+        table=produce_table_with_namespace_column_for_node_ids(table=merges)) \
+        .groupby(cols) \
+        .agg(count=(COLUMN_SOURCE_ID, 'count')) \
+        .reset_index() \
+        .sort_values(COLUMN_COUNT, ascending=False)
+    df["freq"] = df.apply(
+        lambda x: ((x[COLUMN_COUNT] / len(merges)) * 100), axis=1
+    )
+    print_df_stats(df, "merges")
+    return df
+
+def produce_merge_analysis_for_merged_nss_2(merges: DataFrame) -> DataFrame:
+    df = produce_table_with_namespace_column_pair(
+        table=produce_table_with_namespace_column_for_node_ids(table=merges)) \
+        .groupby([get_namespace_column_name_for_column(COLUMN_TARGET_ID)]) \
+        .agg(count=(COLUMN_SOURCE_ID, 'count'),
+             source_nss=(get_namespace_column_name_for_column(COLUMN_SOURCE_ID), lambda x: set(x))) \
+        .reset_index() \
+        .sort_values(COLUMN_COUNT, ascending=False)
+    df["freq"] = df.apply(
+        lambda x: ((x[COLUMN_COUNT] / len(merges)) * 100), axis=1
+    )
+    print_df_stats(df, "merges")
+    return df
+
+
+# PRODUCE & SAVE for ENTITY #
+def produce_and_save_node_analysis(nodes: DataFrame,
+                                   nodes_obs: Union[None, DataFrame],
+                                   mappings: DataFrame,
+                                   edges_hierarchy: DataFrame,
+                                   dataset: str) -> None:
+    produce_node_analysis(nodes=nodes,
+                          mappings=mappings,
+                          edges_hierarchy=edges_hierarchy) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_nodes.csv", index=False)
+    if nodes_obs:
+        produce_node_analysis(nodes=nodes_obs,
+                              mappings=mappings,
+                              edges_hierarchy=edges_hierarchy) \
+            .to_csv(f"{OUTPUT_PATH}/{dataset}_nodes_obsolete.csv", index=False)
+
+
+def produce_and_save_mapping_analysis(mappings: DataFrame, dataset: str) -> None:
+    produce_mapping_analysis_for_prov(mappings=mappings) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_mappings_prov.csv", index=False)
+    produce_mapping_analysis_for_type(mappings=mappings) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_mappings_type.csv", index=False)
+    produce_mapping_analysis_for_mapped_nss(mappings) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_mappings_mapped_nss.csv", index=False)
+    produce_edges_analysis_for_mapped_or_connected_nss_heatmap(mappings, prune=False, directed_edge=False) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_mappings_mapped_nss_heatmap.csv")
+
+
+def produce_and_save_hierarchy_edge_analysis(edges: DataFrame, dataset: str) -> None:
+    produce_hierarchy_edge_analysis_for_mapped_nss(edges=edges) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_hierarchy_edge_connected_nss.csv")
+    produce_edges_analysis_for_mapped_or_connected_nss_heatmap(edges=edges, prune=False, directed_edge=True) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_hierarchy_connected_nss_heatmap.csv", index=True)
+    produce_edges_analysis_for_mapped_or_connected_nss_heatmap(edges=edges, prune=True, directed_edge=True) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_hierarchy_edge_connected_nss_heatmap_pruned.csv", index=True)
+
+
+def produce_and_save_merge_analysis(merges: DataFrame, dataset: str) -> None:
+    produce_merge_analysis_for_merged_nss(merges=merges) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_merges_nss.csv")
+    produce_merge_analysis_for_merged_nss_2(merges=merges) \
+        .to_csv(f"{OUTPUT_PATH}/{dataset}_merges_nss2.csv")
+
+
+# PRODUCE & SAVE for DATASET #
 def analysis_input_dataset():
-    # input_nodes = load_csv(f"{TEST_PATH}/input/nodes.csv")
-    # input_nodes_obs = load_csv(f"{TEST_PATH}/input/nodes_obsolete.csv")[SCHEMA_NODE_ID_LIST_TABLE]
-    # input_mappings = load_csv(f"{TEST_PATH}/input/mappings.csv")
-    # input_edges_hierarchy = load_csv(f"{TEST_PATH}/input/edges_hierarchy.csv")
-    # print_df_stats(input_nodes_obs, "input_nodes_obs")
+    # load
+    input_nodes = load_csv(f"{TEST_PATH}/input/nodes.csv")
+    input_nodes_obs = load_csv(f"{TEST_PATH}/input/nodes_obsolete.csv")[SCHEMA_NODE_ID_LIST_TABLE]
+    input_mappings = load_csv(f"{TEST_PATH}/input/mappings.csv")
+    input_edges_hierarchy = load_csv(f"{TEST_PATH}/input/edges_hierarchy.csv")
 
-    # # nodes
-    # analysis_input_node = produce_node_analysis(nodes=input_nodes,
-    #                                             mappings=input_mappings,
-    #                                             edges_hierarchy=input_edges_hierarchy)
-    # analysis_input_node.to_csv(f"{TEST_PATH}/output/intermediate/analysis/input_nodes.csv")
-    #
-    # # nodes_obs
-    # analysis_input_node_obs = produce_node_analysis(nodes=input_nodes_obs,
-    #                                                 mappings=input_mappings,
-    #                                                 edges_hierarchy=input_edges_hierarchy)
-    # analysis_input_node_obs.to_csv(f"{TEST_PATH}/output/intermediate/analysis/input_nodes_obsolete.csv")
-
-    # mappings: per types, per prov, per mapped, for chart
-    produce_mapping_analysis(mappings=load_csv(f"{TEST_PATH}/input/mappings 2.csv"))
-
-    # edges
+    # analyse and save
+    produce_and_save_node_analysis(nodes=input_nodes,
+                                   nodes_obs=input_nodes_obs,
+                                   mappings=input_mappings,
+                                   edges_hierarchy=input_edges_hierarchy,
+                                   dataset=DIRECTORY_INPUT)
+    produce_and_save_mapping_analysis(mappings=load_csv(f"{TEST_PATH}/input/mappings 2.csv"),
+                                      dataset=DIRECTORY_INPUT)
+    produce_and_save_hierarchy_edge_analysis(edges=input_edges_hierarchy,
+                                             dataset=DIRECTORY_INPUT)
 
 
-analysis_input_dataset()
+def analysis_output_dataset():
+    # load
+
+    output_nodes = load_csv(f"{TEST_PATH_DOM}/nodes.csv")
+    output_mappings = load_csv(f"{TEST_PATH_DOM}/mappings.csv")
+    output_merges = load_csv(f"{TEST_PATH_DOM}/merges.csv")
+    output_edges_hierarchy = load_csv(f"{TEST_PATH_DOM}/edges_hierarchy.csv")
+
+    # analyse and save
+    # produce_and_save_node_analysis(nodes=output_nodes,
+    #                                nodes_obs=None,
+    #                                mappings=output_mappings,
+    #                                edges_hierarchy=output_edges_hierarchy,
+    #                                dataset=DIRECTORY_OUTPUT)
+    # produce_and_save_mapping_analysis(mappings=output_mappings,
+    #                                   dataset=DIRECTORY_OUTPUT)
+    # produce_and_save_hierarchy_edge_analysis(edges=output_edges_hierarchy,
+    #                                          dataset=DIRECTORY_OUTPUT)
+    produce_and_save_merge_analysis(merges=output_merges,
+                                    dataset=DIRECTORY_OUTPUT)
+
+
+analysis_output_dataset()
--- onto_merger/report/plot_generator.py
+++ onto_merger/report/plot_generator.py
@@ -1,8 +1,11 @@
 from typing import List
+import os
 
 import pandas as pd
+from pandas import DataFrame
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
 
 def produce_gantt_chart(file_path: str, data: List[dict]):
@@ -12,7 +15,7 @@ def produce_gantt_chart(file_path: str, data: List[dict]):
         dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30', Duration='10s')
     ])
     fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", text='Duration', width=700,
-                 labels={'Task': ''},)
+                      labels={'Task': ''}, )
     fig.update_layout({
         "plot_bgcolor": "#fff",
         'showlegend': False,
@@ -113,27 +116,27 @@ def produce_horizontal_bar_chart_node_types():
             "t": 10,  # top
             "b": 10,  # bottom
         }
-    }).update_xaxes(visible=False)\
-        .update_yaxes(visible=False)\
+    }).update_xaxes(visible=False) \
+        .update_yaxes(visible=False) \
         .update_traces(textposition='inside', textfont_size=14, textfont_color="white")
     fig.write_image("plotly_horizontal_bar_chart_node_types.svg")
 
 
-def prod_heat_map():
-    idx = ['MONDO', 'MEDDRA', 'MESH', 'DOID']
-    cols = ['MONDO', 'MEDDRA', 'MESH', 'DOID']
-    df = pd.DataFrame([[100, 20, 30, 40],
-                       [50, 100, 8, 15],
-                       [25, 14, 100, 8],
-                       [7, 14, 21, 100]],
-                      columns=cols, index=idx)
-    fig = px.imshow(df, text_auto=True, color_continuous_scale="blues") \
-        .update_xaxes(side="top")
-    fig.write_image("plotly_heat_map.svg")
+def prod_heatmap(data: DataFrame, file_path: str):
+    data2 = data.copy()
+    print(data2)
+    cols = list(data.columns)
+    data2 = data2[cols].replace({'0': np.nan, 0: np.nan})
+    print(data2)
+    fig = px.imshow(data2,
+                    text_auto=True,
+                    color_continuous_scale="blues") \
+        .update_xaxes(side="top") \
+        .update_layout({"plot_bgcolor": "#fff"})
+    fig.write_image(file_path)
     # .update_xaxes(title_font=dict(size=18, family='Courier', color='crimson'))
 
-
-produce_gantt_chart(file_path="plotly_gant_processing", data=[])
+# produce_gantt_chart(file_path="plotly_gant_processing", data=[])
 # produce_vertical_bar_chart_stacked()
 # produce_vertical_bar_chart()
 # produce_horizontal_bar_chart()
